d6969a4d275fbe26be3a3fc678525b55640f39fb
Devin Douglas <djdouglas01@gmail.com>
2023-02-26 12:13:18 +0000

    Change to JS format

--- quiz.js
+++ quiz.js
@@ -1,5 +1,109 @@
 window.onload = function () {
-    (function () {
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+        {
+            question: "What is the tallest mountain in the world?",
+            answers: {
+                A: "Mount Everest",
+                B: "Mount Fuji",
+                C: "K2",
+                D: "Mount Kilimanjaro"
+            },
+            correctAnswer: "A"
+        },
+        {
+            question: "What is the capital of New Zealand?",
+            answers: {
+                A: "Auckland",
+                B: "Christchurch",
+                C: "Wellington",
+                D: "Dunedin"
+            },
+            correctAnswer: "C"
+        },
+        {
+            question: "What is the native flightless bird to New Zealand?",
+            answers: {
+                A: "Ostrich",
+                B: "Emu",
+                C: "Emperor Penguin",
+                D: "Kiwi",
+            },
+            correctAnswer: "D"
+        },
+        {
+            question: "What country has the longest coastline in the world?",
+            answers: {
+                A: "Indonesia",
+                B: "Brazil",
+                C: "Canada",
+                D: "Australia",
+            },
+            correctAnswer: "C"
+        },
+        {
+            question: "What is the Capital of Peru?",
+            answers: {
+                A: "Bogota",
+                B: "Lima",
+                C: "London",
+                D: "Buenos Aires",
+            },
+            correctAnswer: "B"
+        },
+        {
+            question: "How long is the border between USA and Canada?",
+            answers: {
+                A: "5,891 km",
+                B: "6,891 km",
+                C: "7,891 km",
+                D: "8,891 km",
+            },
+            correctAnswer: "D"
+        },
+        {
+            question: "Which river flows through London",
+            answers: {
+                A: "River Severn",
+                B: "River Trent",
+                C: "River Thames",
+                D: "Danube River",
+            },
+            correctAnswer: "C"
+        },
+        {
+            question: "On which continent is the Sahara Desert located?",
+            answers: {
+                A: "Africa",
+                B: "South America",
+                C: "Asia",
+                D: "Europe",
+            },
+            correctAnswer: "A"
+        },
+        {
+            question: "What is the smallest country in the world?",
+            answers: {
+                A: "Andorra",
+                B: "Luxemburg",
+                C: "Vatican City",
+                D: "Belgium",
+            },
+            correctAnswer: "C"
+        },
+        {
+            question: "Which European city is not located on the Danube River",
+            answers: {
+                A: "Prague",
+                B: "Budapest",
+                C: "Vienna",
+                D: "Belgrade",
+            },
+            correctAnswer: "A"
+        },
+    ];
         function buildQuiz() {
 
             const output = [];
@@ -99,111 +203,7 @@ window.onload = function () {
             showSlide(currentSlide - 1);
         }
 
-        const quizContainer = document.getElementById('quiz');
-        const resultsContainer = document.getElementById('results');
-        const submitButton = document.getElementById('submit');
-        const myQuestions = [
-            {
-                question: "What is the tallest mountain in the world?",
-                answers: {
-                    a: "Mount Everest",
-                    b: "Mount Fuji",
-                    c: "K2",
-                    d: "Mount Kilimanjaro"
-                },
-                correctAnswer: "a"
-            },
-            {
-                question: "What is the capital of New Zealand?",
-                answers: {
-                    a: "Auckland",
-                    b: "Christchurch",
-                    c: "Wellington",
-                    d: "Dunedin"
-                },
-                correctAnswer: "c"
-            },
-            {
-                question: "What is the native flightless bird to New Zealand?",
-                answers: {
-                    a: "Ostrich",
-                    b: "Emu",
-                    c: "Emperor Penguin",
-                    d: "Kiwi",
-                },
-                correctAnswer: "d"
-            },
-            {
-                question: "What country has the longest coastline in the world?",
-                answers: {
-                    a: "Indonesia",
-                    b: "Brazil",
-                    c: "Canada",
-                    d: "Australia",
-                },
-                correctAnswer: "c"
-            },
-            {
-                question: "What is the Capital of Peru?",
-                answers: {
-                    a: "Bogota",
-                    b: "Lima",
-                    c: "London",
-                    d: "Buenos Aires",
-                },
-                correctAnswer: "b"
-            },
-            {
-                question: "How long is the border between USA and Canada?",
-                answers: {
-                    a: "5,891 km",
-                    b: "6,891 km",
-                    c: "7,891 km",
-                    d: "8,891 km",
-                },
-                correctAnswer: "d"
-            },
-            {
-                question: "Which river flows through London",
-                answers: {
-                    a: "River Severn",
-                    b: "River Trent",
-                    c: "River Thames",
-                    d: "Danube River",
-                },
-                correctAnswer: "c"
-            },
-            {
-                question: "On which continent is the Sahara Desert located?",
-                answers: {
-                    a: "Africa",
-                    b: "South America",
-                    c: "Asia",
-                    d: "Europe",
-                },
-                correctAnswer: "a"
-            },
-            {
-                question: "What is the smallest country in the world?",
-                answers: {
-                    a: "Andorra",
-                    b: "Luxemburg",
-                    c: "Vatican City",
-                    d: "Belgium",
-                },
-                correctAnswer: "c"
-            },
-            {
-                question: "Which European city is not located on the Danube River",
-                answers: {
-                    a: "Prague",
-                    b: "Budapest",
-                    c: "Vienna",
-                    d: "Belgrade",
-                },
-                correctAnswer: "a"
-            },
-        ];
+
 
 
         buildQuiz();
@@ -220,18 +220,22 @@ window.onload = function () {
         previousButton.addEventListener("click", showPreviousSlide);
         nextButton.addEventListener("click", showNextSlide);
 
-    })();
+        var questionTimerLimit = 15;
+        var count = questionTimerLimit;
+        var interval = setInterval(function () {
+            count--;
+            document.getElementById('safeTimer').innerHTML = count;
+            if (count === 0) {
+                showNextSlide();
+                count = questionTimerLimit;
+              //  clearInterval(interval);
+               document.getElementById('safeTimer').innerHTML = 'Time is up!';
+            }
+        }, 1000);
+    
 
 }
 
-var count = 15;
-var interval = setInterval(function () {
-    document.getElementById('safeTimer').innerHTML = count;
-    count--;
-    if (count === 0) {
-        clearInterval(interval);
-        document.getElementById('safeTimer').innerHTML = 'Done';
-    }
-}, 1000);
+
 
 
